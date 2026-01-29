4efdd2c7d432efa9874fb21bf3b31914a05706c9
Jaesuru <jasongsm2017@gmail.com>
2026-01-29 17:41:02 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let num1;
+let calOp;
+let num2;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
