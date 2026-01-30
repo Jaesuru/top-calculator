2fb9b5fe08bbf3b9c0d963229a8b935169423493
Jaesuru <jasongsm2017@gmail.com>
2026-01-30 16:15:02 -0500

    Add display update and basic script functionality
    Ensure that when clicking the buttons on the numbers
    it will update the display screen and also properly
    handle any decimals needed.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let num1;
-let calOp;
-let num2;
+let input1;
+let operator;
+let input2;
 
 function add(a, b) {
     return a + b;
@@ -16,4 +16,49 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(num1, calOp, num2) {
+    let result = calOp(num1, num2);
+    return result;
+}
+
+const display = document.querySelector(".display");
+
+const numButtons = document.querySelectorAll(`.numBtns button`);
+
+let currentNumber = ``;
+
+function updateDisplay(digit) {
+    if (currentNumber === "0") {
+        currentNumber = digit;
+    } else {
+        currentNumber += digit;
+    }
+
+    display.textContent = currentNumber;
+}
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'AC') {
+            clearDisplay();
+        } else if (button.textContent === '.') {
+            handleDecimal();
+        } else {
+            updateDisplay(button.textContent);
+        }
+    });
+});
+
+function clearDisplay() {
+    currentNumber = "0";
+    display.textContent = currentNumber;
+}
+
+function handleDecimal() {
+    if (!currentNumber.includes(".")) {
+        currentNumber += ".";
+        display.textContent = currentNumber;
+    }
 }
