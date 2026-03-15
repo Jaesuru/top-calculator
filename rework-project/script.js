9e1f6eb3b006d87e0d37a1da013d7465aeb4cf91
Jaesuru <jasongsm2017@gmail.com>
2026-03-15 19:37:45 -0400

    Add Essential Core Logic of Calculator
    Basic functions of the calculator added such as:
    - Input/Output of numbers
    - Basic operations
    - Clearing
    
    Still need to implement decimal system, negative,
    and chaining.

--- rework-project/script.js
+++ rework-project/script.js
@@ -1,3 +1,82 @@
-function updateDisplay() {
-    
+const display = document.querySelector(".calc-display");
+const buttons = document.querySelectorAll(".btns button");
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+let resetDisplay = false;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click",() => {
+        const value = button.textContent;
+
+        if (!isNaN(value)) {
+            appendNumber(value);
+        } else if (value === "=") {
+            evaluate();
+        } else if (value === "AC") {
+            clearDisplay();
+        } else {
+            setOperator(value);
+        }
+    })
+})
+
+function operate(a, op, b) {
+    a = Number(a);
+    b = Number(b);
+
+    if (op === "+") return add(a, b);
+    if (op === "-") return subtract(a, b);
+    if (op === "÷") return divide(a, b);
+    if (op === "x") return multiply(a, b);
+}
+
+function appendNumber(num) {
+    if (display.textContent === "0" || resetDisplay) {
+        display.textContent = num;
+        resetDisplay = false; 
+    } else {
+        display.textContent += num;
+    }
+}
+
+function setOperator(op) {
+    firstNumber = display.textContent;
+    operator = op;
+    resetDisplay = true;
+}
+
+function evaluate() {
+    secondNumber = display.textContent;
+
+    const result = operate(firstNumber, operator, secondNumber);
+
+    display.textContent = result;
+
+    firstNumber = result;
+    operator = "";
+}
+
+function clearDisplay() {
+    display.textContent = "0";
+    firstNumber = "";
+    operator = "";
+    secondNumber = "";
 }
